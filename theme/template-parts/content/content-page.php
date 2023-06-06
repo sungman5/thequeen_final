907a9e96f6666da79850b245604767b3c0f1c7c6
sungman5 <sungman5@gmail.com>
2023-06-06 21:39:15 +0900

    first commit

---
+++ theme/template-parts/content/content-page.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Template part for displaying pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package thequeenkorea_4
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+		<?php
+		if ( ! is_front_page() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} else {
+			the_title( '<h2 class="entry-title">', '</h2>' );
+		}
+		?>
+	</header><!-- .entry-header -->
+
+	<?php thequeenkorea_4_post_thumbnail(); ?>
+
+	<div <?php thequeenkorea_4_content_class( 'entry-content' ); ?>>
+		<?php
+		the_content();
+
+		wp_link_pages(
+			array(
+				'before' => '<div>' . __( 'Pages:', 'thequeenkorea_4' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+	</div><!-- .entry-content -->
+
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers. */
+						__( 'Edit <span class="sr-only">%s</span>', 'thequeenkorea_4' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				)
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
+
+</article><!-- #post-<?php the_ID(); ?> -->
